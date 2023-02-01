b43636a567f8aa5fdba9ac72fd4882148a65f4a3
Dario <dario.ferrai>
2023-02-01 17:34:13 +0200

    Intercepting parser to use IntAsEnumODataUriResolver in ODataService

--- ODataService.cs
+++ ODataService.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using CodeNotion.Odata.Resolvers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
 
 namespace CodeNotion.Odata;
 
@@ -14,6 +20,7 @@ public class ODataService
 {
     private readonly ODataQuerySettings _settings;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private static readonly FieldInfo? ParserField = typeof(ODataQueryOptions).GetField("_queryOptionParser", BindingFlags.NonPublic | BindingFlags.Instance);
 
     public ODataService(ODataQuerySettings settings, IHttpContextAccessor httpContextAccessor)
     {
@@ -23,6 +30,7 @@ public class ODataService
 
     public async Task<ManagedPageResult<TEntity>> ToPagedResultAsync<TEntity>(IQueryable<TEntity> source, ODataQueryOptions<TEntity> queryOptions)
     {
+        InterceptParser(queryOptions);
         var query = ApplyOdata(source, queryOptions);
 
         var count = await GetCount(source, queryOptions);
@@ -37,12 +45,12 @@ public class ODataService
         {
             return Array.Empty<TEntity>();
         }
-        
+
         if (query is IAsyncEnumerable<TEntity>)
         {
             return await query.ToArrayAsync(_httpContextAccessor.HttpContext!.RequestAborted);
         }
-        
+
         return query.ToArray();
     }
 
@@ -53,7 +61,7 @@ public class ODataService
             return default;
         }
 
-        var countSource = (IQueryable<TEntity>?)queryOptions.Filter?.ApplyTo(source, _settings) ?? source;
+        var countSource = (IQueryable<TEntity>?) queryOptions.Filter?.ApplyTo(source, _settings) ?? source;
         if (countSource is IAsyncEnumerable<TEntity>)
         {
             return await countSource.LongCountAsync(_httpContextAccessor.HttpContext!.RequestAborted);
@@ -64,13 +72,30 @@ public class ODataService
 
     private IQueryable<TEntity> ApplyOdata<TEntity>(IQueryable<TEntity> source, ODataQueryOptions<TEntity> queryOptions)
     {
-        var query = (IQueryable)source;
+        var query = (IQueryable) source;
         query = queryOptions.OrderBy?.ApplyTo(query, _settings) ?? query;
         query = queryOptions.Filter?.ApplyTo(query, _settings) ?? query;
         query = queryOptions.Apply?.ApplyTo(query, _settings) ?? query;
         query = queryOptions.SelectExpand?.ApplyTo(query, _settings) ?? query;
         query = queryOptions.Skip?.ApplyTo(query, _settings) ?? query;
         query = queryOptions.Top?.ApplyTo(query, _settings) ?? query;
-        return (IQueryable<TEntity>)query;
+        return (IQueryable<TEntity>) query;
+    }
+
+    /// <summary>
+    /// We intercept the parser to allow the injection of the IntAsEnumODataUriResolver
+    /// </summary>
+    private static void InterceptParser<TEntity>(ODataQueryOptions<TEntity> queryOptions)
+    {
+        var parser = (ODataQueryOptionParser?) ParserField?.GetValue(queryOptions);
+        if (parser == null)
+        {
+            throw new InvalidOperationException("Could not get ODataQueryOptionParser from ODataQueryOptions");
+        }
+
+        parser.Resolver = new IntAsEnumODataUriResolver(new ODataUriResolver())
+        {
+            EnableCaseInsensitive = true
+        };
     }
 }
